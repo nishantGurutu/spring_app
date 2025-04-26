38312c8d40bd82ecf9f00cd031b2c15db819115b
Nishant Kumar <this.is.nishantkumar1@gmail.com>
2025-04-27 02:58:16 +0530

    first init

---
+++ src/main/java/com/example/attendence/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.example.attendence;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to spring boot";
+    }
+}
